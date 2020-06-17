9e82ecb49e97156fadc47a0f60300271f7105628
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-06-17 17:08:55 +0530

    day-6, project-3
    work on the script file

--- Reviews/script.js
+++ Reviews/script.js
@@ -1,1 +1,6 @@
-// script file
+ // script file
+
+ // Implement carausel logic.
+ // Set object for each person's photo, name, profession, review.
+ // Implement the next and prev buttons.
+ // Implement the random person's review generator.
