c9a040af4c95f8add368205ebef2e3d9ab4d828c
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-06-22 10:14:57 +0530

    day-10 project-5
    12 hour format added

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,3 +1,7 @@
+function _24hrs(){
+	document.getElementById('_24').style.backgroundColor="#ea907a";
+	document.getElementById('_12').style.backgroundColor="#fff";
+	document.getElementById('section').style.display="none";	
 
 function clock(){
 	let d=new Date();
@@ -10,3 +14,29 @@ function clock(){
 	document.getElementById('secs').innerText=seconds;
 }
 setInterval(clock,1000);
+}
+
+	// IMPLEMENT AM/PM FEATURE ALONG WITH 12 HOUR SYSTEM, AND 24 HOUR SYSTEM TOGGLE-ABLE
+function _12hrs(){
+	document.getElementById('_24').style.backgroundColor="#fff"
+	document.getElementById('_12').style.backgroundColor="#ea907a";
+	document.getElementById('section').style.display="inline";	
+function clock(){
+	let d=new Date();
+	let mode="AM";
+	let hours=d.getHours();
+	if(hours>=13)
+		mode="PM";
+	hours=hours%12;
+	let minutes=d.getMinutes();
+	let seconds=d.getSeconds();
+	// console.log(`${hours}:${minutes}:${seconds}`);
+	console.log('this is the pm format');
+	document.getElementById('hrs').innerText=hours+':';
+	document.getElementById('mins').innerText=minutes+':';
+	document.getElementById('secs').innerText=seconds;
+	document.getElementById('section').innerText=mode;
+}
+setInterval(clock,1000);
+}
+
