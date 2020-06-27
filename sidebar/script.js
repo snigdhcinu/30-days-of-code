4fbc98627139f880d536ce9534b5052805bdd05a
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-06-28 01:20:59 +0530

    Day-12 project-6
    everything is alright

--- sidebar/script.js
+++ sidebar/script.js
@@ -2,16 +2,24 @@ let i=0;
 function toggle(){
 	if(i==0){
 		document.getElementById('sidebar').style.display='block';
+		document.getElementById('sidebar').style.backgroundColor='#fff';
+		document.getElementById('top-right').style.backgroundColor="#fff";
+
+
 		document.getElementById('toggler').style.boxShadow="2px 2px 2px 2px #e8ead3";
 		i=1;
 	}
 	else if(i==1){
 		document.getElementById('sidebar').style.display='none';
 		document.getElementById('toggler').style.boxShadow="2px 2px 2px 2px #ddf3f5";
+		
+
 		i=0;
 	}
 }
 function hide(){
 	document.getElementById('sidebar').style.display='none';
+	document.getElementById('toggler').style.boxShadow="2px 2px 2px 2px #ddf3f5";
+
 	i=0;
 }
