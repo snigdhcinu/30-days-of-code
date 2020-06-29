af3929974dadbab623cc383766dfa32fc730676b
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-06-29 22:53:07 +0530

    improvement needed

--- modal/script.js
+++ modal/script.js
@@ -3,7 +3,9 @@ function toggle(){
 	if(!i){
 		document.getElementById('modal').style.visibility='visible';
 		i=1;
-		document.getElementsByTagName('body')[0].style.backgroundColor='#e4f9ff';
+		document.getElementsById('body').style.backgroundColor='#e4f9ff';
+		// document.getElementById('body').style.filter="opacity(0.5)";
+
 	}
 	else if(i){
 		document.getElementById('modal').style.visibility='hidden';
