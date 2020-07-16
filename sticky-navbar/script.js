8051a7dd7509fa45bbf8eb4f6d511c16b98f5a3e
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-07-16 20:42:18 +0530

    Day-20 project-10
    Done, based on how to make navigation section fixed, and on window object properties and methods

--- sticky-navbar/script.js
+++ sticky-navbar/script.js
@@ -1,1 +1,15 @@
 // This is the script file.
+function stick(){
+	document.getElementById('nav-bar').style.position='fixed';
+	document.getElementById('nav-bar').style.backgroundColor='#f0ece3';
+	document.getElementById('nav-bar').style.opacity='0.7';
+
+	if(window.pageYOffset==0){
+		// console.log('back to top');
+	document.getElementById('nav-bar').style.backgroundColor='#d3dbff';
+	document.getElementById('nav-bar').style.opacity='1';
+	}
+
+}
+window.onscroll=function(){stick()};
+
