d32d8918c96e1f78fb2400d9c1a1bc8bf6505a71
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-07-21 14:23:16 +0530

    Day-24 project-12
    Look into responsiveness

--- countdown/script.js
+++ countdown/script.js
@@ -1,3 +1,49 @@
 // this is the script file.
 // set initial time
 // can use browser time, or initialize a timer of our own
+// Sale ends on 29/7/2020	9PM
+let DD=29;
+let MM=7;
+let HH=21;
+let MM1=60;
+let SS=60;
+let d=new Date();
+let day=d.getDate();
+let hr=d.getHours();
+let min=d.getMinutes();
+let sec=d.getSeconds();
+
+setInterval(counter,1000);
+function counter(){
+	let DD=29;
+	let MM=7;
+	let HH=21;
+	let MM1=60;
+	let SS=60;
+	let d=new Date();
+	let day=d.getDate();
+	let hr=d.getHours();
+	let min=d.getMinutes();
+	let sec=d.getSeconds();
+	let num=0
+	num=Math.abs(DD-day)
+	if(num<10){
+		num='0'+num;
+	}
+	document.getElementById('days').innerText=num;
+		num=Math.abs(HH-hr)
+	if(num<10){
+		num='0'+num;
+	}
+	document.getElementById('hrs').innerText=num;
+		num=Math.abs(MM1-min)
+	if(num<10){
+		num='0'+num;
+	}
+	document.getElementById('mins').innerText=num;
+		num=Math.abs(SS-sec)
+	if(num<10){
+		num='0'+num;
+	}
+	document.getElementById('secs').innerText=num;
+}
