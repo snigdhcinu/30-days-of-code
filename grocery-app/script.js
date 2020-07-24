c442efaa5227915dd14f20b2052e4eae7ddf77f3
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-07-24 21:51:45 +0530

    adding crud operations
    learnt importance of functions and innerHTML, innerText.
    Work on storing the data into localStorage, by creating CRUD functions, manipulating the localStorage

--- grocery-app/script.js
+++ grocery-app/script.js
@@ -1,7 +1,32 @@
  // This is the script file
 let list=document.getElementById('lists');
+let i=0;
+
+
+
+ function delet(n){
+ 	let node=document.getElementById("list-item-"+n);
+ 	// console.log(node.innerText)
+ 	let btn_e=node.nextSibling;
+ 	let btn_d=btn_e.nextSibling;
+ 	list.removeChild(btn_d);
+ 	list.removeChild(btn_e);
+ 	list.removeChild(node);
+
+
+ }
+ function edit(n){
+ 	let node=document.getElementById("list-item-"+n);
+ 	// console.log(node.innerText)
+ 	let text=prompt('Enter the updated value for'+' '+node.innerText);
+ 	node.innerText=text;
+ }
+ function delete_all(){
+ 	list.innerHTML=" ";
+ }
  function add(){
  	let input=document.getElementsByTagName('input')[0].value;
- 	list.innerHTML+="<li class='list-item'>"+input+"</li><button class='edit-btn'>✏️</button><button class'delete-btn'>🗑</button>"
+ 	i+=1;
+ 	list.innerHTML+="<li class='list-item' id='list-item-"+i+"'>"+input+"</li><button class='edit-btn' id='edit-btn-"+i+"' onclick='edit("+i+")'>✏️E</button><button class='delete-btn' id='delete-btn-"+i+"' onclick='delet("+i+")'>🗑D</button>"
  	document.getElementsByTagName('input')[0].value=' ';
  }
