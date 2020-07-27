0692a48154d41fc96064559440df8acc6ae20ec1
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-07-27 19:25:11 +0530

    day-30 project-15
    Learnt about css transition property.
    Very very sexy thing

--- slide-show/script.js
+++ slide-show/script.js
@@ -1,1 +1,21 @@
  // this is the script file
+ let i=1;
+ let len=4; // Total no. of available images/slide elements.
+ let container=document.getElementById('container');
+ function prev(){
+ 	if(i==1)
+ 		i=len;
+ 	else
+ 		i-=1;
+ 	container.style.backgroundImage="url('public/pic"+i+".jpg')";
+ 	console.log(container.style.backgroundImage)
+ }
+ function next(){
+ 	if(i==len)
+ 		i=1;
+ 	else
+ 		i+=1;
+ 	container.style.backgroundImage="url('public/pic"+i+".jpg')";
+ 	 	console.log(container.style.backgroundImage)
+ }
+ setInterval(next,5000);
