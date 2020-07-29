2674fed1b01672061f3718246f1b078ba0e192a5
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-07-29 14:23:27 +0530

    removing brightness bug; improving  responsiveness

--- modal/script.js
+++ modal/script.js
@@ -3,16 +3,21 @@ function toggle(){
 	if(!i){
 		document.getElementById('modal').style.visibility='visible';
 		i=1;
-		document.getElementsById('body').style.backgroundColor='#e4f9ff';
-		// document.getElementById('body').style.filter="opacity(0.5)";
+		document.getElementById('body').style.backgroundColor='#e4f9ff';
+		document.getElementById('body').style.filter="brightness(0.5)";
+		document.getElementsByTagName('button')[1].innerText='Close Modal'
 
 	}
 	else if(i){
 		document.getElementById('modal').style.visibility='hidden';
+		document.getElementsByTagName('button')[1].innerText='Open Modal'
+		document.getElementById('body').style.filter="brightness(1)"
 		i=0;
 	}
 }
 function hide(){
 	document.getElementById('modal').style.visibility='hidden';
+	document.getElementsByTagName('button')[1].innerText='Open Modal'
+	document.getElementById('body').style.filter="brightness(1)"
 	i=0;
 }
